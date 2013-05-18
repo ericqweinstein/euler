76dbd47bd00892ff3a731ddbca81e18ba7d7c87b
Eric Weinstein <eric.q.weinstein@gmail.com>
2013-05-18 16:30:29 -0400

    fix index

--- haskell_solutions/seven.hs
+++ haskell_solutions/seven.hs
@@ -3,12 +3,12 @@
 -- What is the 10,001st prime number?
 
 problemSeven :: Int -> Integer
-problemSeven n = last (take n [x | x <- [1..], isPrime x])
+problemSeven n = last (take (n+1) [x | x <- [1..], isPrime x])
 
 isPrime :: Integer -> Bool
 isPrime n = not $ any divisible $ takeWhile dividing [2..] where
   divisible x = n `mod` x == 0
   dividing  x = x * x <= n
 
--- problemSeven 10002 ==> 104743
+-- problemSeven 10001 ==> 104743
 
