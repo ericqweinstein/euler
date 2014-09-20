4eeb4b0e1a2963ca0cdf2e7a4d561a58f04c7ded
Eric Weinstein <eric.q.weinstein@gmail.com>
2014-09-20 18:47:58 -0400

    Update dependencies

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,9 @@
 (defproject euler "0.1.0"
   :description "Project Euler solutions in Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [net.mikera/core.matrix "0.29.1"]]
+  :profiles {:dev {:plugins [[lein-kibit "0.0.8"]
+                             [lein-bikeshed "0.1.8"]]}}
+  :aliases {"lint" ^{:doc "Lint Clojures."}
+            ["do" "kibit," "bikeshed" "-m1000"]})
